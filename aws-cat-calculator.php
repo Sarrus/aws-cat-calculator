66f6720e838d3da59fe44dee2fd4b526b46a8bfc
Michael Bell <michael@tertheya.com>
2021-11-26 15:36:06 +0000

    Added argument parsing

--- aws-cat-calculator.php
+++ aws-cat-calculator.php
@@ -1,1 +1,12 @@
 <?php
+
+foreach(getopt("h") as $option => $argument)
+{
+    switch($option)
+    {
+        case 'h':
+            fprintf(STDERR, "Usage: php aws-cat-calculator.php [options]\r\n" .
+                "  -h  Display this help.");
+            break;
+    }
+}
